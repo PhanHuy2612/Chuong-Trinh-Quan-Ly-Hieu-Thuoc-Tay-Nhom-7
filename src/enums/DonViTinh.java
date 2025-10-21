9b64e085e53d0ecc738a094e171be469a23accd7
NguyenTranTrongHuy2912 <23671451.huy@student.iuh.edu.vn>
2025-10-21 19:39:13 +0700

    feat: add KhoHang entity and DonViTinh enum, update project configuration

---
+++ src/enums/DonViTinh.java
@@ -0,0 +1,18 @@
+package enums;
+
+public enum DonViTinh {
+    VIEN("Viên"),
+    HOP("Hộp"),
+    VI("Vỉ"),
+    CHAI("Chai");
+
+    private final String moTa;
+
+    DonViTinh(String moTa) {
+        this.moTa = moTa;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+}
