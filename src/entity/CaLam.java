133cdca54c1c098d03a71b65db14eb6a83e6ddc9
NguyenTranTrongHuy2912 <23671451.huy@student.iuh.edu.vn>
2025-10-21 20:21:51 +0700

    feat: add CaLam entity with fields, methods, and overrides for toString, equals, and hashCode

--- src/entity/CaLam.java
+++ src/entity/CaLam.java
@@ -1,4 +1,76 @@
 package entity;
 
+import java.util.Objects;
+
 public class CaLam {
+    private String maCa;
+    private String tenCa;
+    private String thoiGianBatDau;
+    private String thoiGianKetThuc;
+
+    public CaLam() {
+    }
+
+    public CaLam(String maCa, String tenCa, String thoiGianBatDau, String thoiGianKetThuc) {
+        this.maCa = maCa;
+        this.tenCa = tenCa;
+        this.thoiGianBatDau = thoiGianBatDau;
+        this.thoiGianKetThuc = thoiGianKetThuc;
+    }
+
+    public String getMaCa() {
+        return maCa;
+    }
+
+    public void setMaCa(String maCa) {
+        this.maCa = maCa;
+    }
+
+    public String getTenCa() {
+        return tenCa;
+    }
+
+    public void setTenCa(String tenCa) {
+        this.tenCa = tenCa;
+    }
+
+    public String getThoiGianBatDau() {
+        return thoiGianBatDau;
+    }
+
+    public void setThoiGianBatDau(String thoiGianBatDau) {
+        this.thoiGianBatDau = thoiGianBatDau;
+    }
+
+    public String getThoiGianKetThuc() {
+        return thoiGianKetThuc;
+    }
+
+    public void setThoiGianKetThuc(String thoiGianKetThuc) {
+        this.thoiGianKetThuc = thoiGianKetThuc;
+    }
+
+    @Override
+    public String toString() {
+        return "CaLam{" +
+                "maCa='" + maCa + '\'' +
+                ", tenCa='" + tenCa + '\'' +
+                ", thoiGianBatDau='" + thoiGianBatDau + '\'' +
+                ", thoiGianKetThuc='" + thoiGianKetThuc + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CaLam caLam = (CaLam) o;
+        return Objects.equals(maCa, caLam.maCa);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(maCa);
+    }
+
+
 }
